935e20f3e0c1c1e90ecf0786f9575e8d71add339
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-03-20 01:08:12 -0400

    testing code for the trhee dumb sorts

---
+++ sortTest.js
@@ -0,0 +1,8 @@
+load("carray.js");
+var numElements = 10;
+var mynums = new CArray(numElements);
+mynums.setData();
+print(mynums.toString());
+mynums.insertionSort();
+print();
+print(mynums.toString());
