f872248996d8b2b8d35526c5f8c239bd6c71e43f
Ryan Postma <rpw10104@saintjoe.edu>
2015-03-23 02:16:50 -0400

    Delete sortTest.js

--- sortTest.js
+++
@@ -1,8 +0,0 @@
-load("carray.js");
-var numElements = 10;
-var mynums = new CArray(numElements);
-mynums.setData();
-print(mynums.toString());
-mynums.insertionSort();
-print();
-print(mynums.toString());
