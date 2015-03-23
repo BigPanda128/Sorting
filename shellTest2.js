a1bed06b23d2b86ffed6af9d200088b5e881da15
Ryan Postma <rpw10104@saintjoe.edu>
2015-03-23 02:16:32 -0400

    Delete shellTest2.js

--- shellTest2.js
+++
@@ -1,8 +0,0 @@
-load("carray.js")
-var nums = new CArray(100);
-nums.setData();
-print("Before Shellsort2: \n");
-print(nums.toString());
-nums.shellsort2();
-print("\nAfter Shellsort2: \n");
-print(nums.toString());
